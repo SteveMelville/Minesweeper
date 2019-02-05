750fce7568aeb0e48b35f5a2310c511678df432f
Steven Melville <steven.melville@ad.snow.edu>
2019-02-05 15:17:03 -0700

    Got basic minesweeper running!

--- Minesweeper.py
+++ Minesweeper.py
@@ -15,8 +15,9 @@ class cell:
         self.rect = pygame.Rect(20*x, 20*y, 20, 20)
         self.type = 1
         self.isMine = False
-        self.text
-        self.testrect
+        self.numMines = 0
+        self.x = x
+        self.y = y
         
     def click(self, pos, button):
         if self.isClicked == False:
@@ -29,20 +30,24 @@ class cell:
                 elif button == 2:
                     self.isClicked = False
                 
-    def sweep(self, basicfont):
+    def sweep(self):
         if self.isMine:
             return 0
         else:
-            text = basicfont.render(str(num), True, colors[3], colors[0])
             return 4
         
-    def draw(self, screen):
+    def draw(self, screen, text, textrect):
+        textrect.centerx = self.x * 20 + 10
+        textrect.centery = self.y * 20 + 10
+        
         pygame.draw.rect(screen, colors[self.type], self.rect)
-        screen.blit(self.text, self.textrect)
         pygame.draw.rect(screen, (128,128,128), self.rect, 2)
+        if self.isClicked and not self.isMine:
+            screen.blit(text, textrect)
         
 def isWinner(board):
     return 0
+
 
 
 def generateBoard(board, width, height, numSquares, mines):
@@ -56,6 +61,19 @@ def generateBoard(board, width, height, numSquares, mines):
             else:
                 board[x][y].isMine = False
                 numSquares = numSquares - 1
+                
+    for x in range(0,width):
+        for y in range(0, height):
+            count = 0
+            if not board[x][y].isMine:
+                for i in range(-1, 2):
+                    for j in range(-1, 2):
+                        if i + x < width and j + y < height and i + x >= 0 and j + y >= 0:
+                            if board[x + i][y + j].isMine:
+                                count = count + 1
+    
+                board[x][y].numMines = count
+                
     
     return board
 
@@ -66,7 +84,7 @@ def main(xwidth, yheight, mines):
     size = width, height = xwidth * 20, yheight * 20
     screen = pygame.display.set_mode(size)
     
-    basicfont = pygame.font.SysFont(None, 48)
+    basicfont = pygame.font.SysFont(None, 24)
     
     squares = [[cell(x,y) for y in range(0,yheight)] for x in range(0,xwidth)]
     
@@ -94,7 +112,9 @@ def main(xwidth, yheight, mines):
         
         for x in range(0,xwidth):
             for y in range(0,yheight):
-                squares[x][y].draw(screen)
+                text = basicfont.render(str(squares[x][y].numMines), True, colors[3], colors[4])
+                textrect = text.get_rect()
+                squares[x][y].draw(screen, text, textrect)
                 
         
         
@@ -103,4 +123,4 @@ def main(xwidth, yheight, mines):
     
 
         
-main(5, 5, 10)
+main(10, 10, 30)
